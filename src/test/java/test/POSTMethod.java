d0d7de7abf3d7d3552bb1cd5a7af3a54b69edf01
Chi Dương <chidl@ChiDLs-MacBook-Air.local>
2023-05-12 01:10:02 +0700

    first commit

---
+++ src/test/java/test/POSTMethod.java
@@ -0,0 +1,47 @@
+package test;
+import com.google.gson.Gson;
+import io.restassured.http.Header;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import model.PostBody;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.equalTo;
+
+public class POSTMethod {
+    public static void main(String[] args) {
+        //Request specification object
+        String baseUri = "https://jsonplaceholder.typicode.com/";
+        RequestSpecification req = given();
+        req.baseUri(baseUri);
+
+        //Content-type => Header
+        req.header(new Header("Content-type", "application/json; charset=UTF-8"));
+
+        //Form up request body
+//        String postBoddy = "{\n" +
+//                "  \"userId\": 1,\n" +
+//                "  \"id\": 1,\n" +
+//                "  \"title\": \"new title\",\n" +
+//                "  \"body\": \"new body\"}";
+
+        //Gson
+        Gson gson = new Gson();
+        PostBody postBody = new PostBody();
+        postBody.setUserId(1);
+        postBody.setTitle("new title1");
+        postBody.setBody("new body1");
+
+        //Send POST request
+        Response res = req.body(gson.toJson(postBody)).post("/posts");
+        res.prettyPrint();
+
+        //Verification
+        res.then().statusCode(201);
+        res.then().statusLine(containsString("201 Created"));
+        res.then().body("userId", equalTo(1));
+        res.then().body("title", equalTo("new title1"));
+        res.then().body("body", equalTo("new body1"));
+    }
+}
---
+++ src/test/java/test/SimpleTest.java
@@ -0,0 +1,28 @@
+package test;
+
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.equalTo;
+
+public class SimpleTest {
+    public static void main(String[] args) {
+        String baseUri = "https://jsonplaceholder.typicode.com/";
+
+        //request scope
+        RequestSpecification req = given();
+        req.baseUri(baseUri);
+        req.basePath("/todos");
+
+        //responsive
+        final String FIRST_TODO = "/1";
+        Response res = req.get(FIRST_TODO);
+        res.prettyPrint();
+
+        //verify
+
+        res.then().body("userId", equalTo(1));
+        res.then().body("title", equalTo("delectus aut autem"));
+        res.then().body("completed", equalTo(true));
+    }
+}
